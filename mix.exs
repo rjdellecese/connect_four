70113624dd469646f297d73da227babc265f7049
RJ Dellecese <rjdellecese@gmail.com>
2019-09-07 14:31:22 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ConnectFour.MixProject do
   def project do
     [
       app: :connect_four,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
